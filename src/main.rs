1a837e11be89ee5bc8ce8cfbeb9654206d65e7ec
Satyam Singh <crackcoder@outlook.com>
2024-12-12 08:39:17 +0530

    Debug and Display

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::fmt;
+use std::fmt::Display;
+
+/// Person struct
+#[derive(Debug)]
+struct Person {
+    age: i32,
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.age)
+    }
+}
+
 fn main() {
     println!("Hello World");
     println!("I'm a Crustacean");
+
+    let person = Person { age: 18 };
+    println!("{}", person);
 }
